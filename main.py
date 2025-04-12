7ba6bedca7cd84e29f6d019f5f80314e5a6ba1d6
samedit66 <samedit66@yandex.ru>
2025-04-12 04:24:16 +0300

    feat: add cli interface

--- main.py
+++ main.py
@@ -1,12 +1,40 @@
-from django_logs_analyzer.parse_logs import parse_logs
+from argparse import ArgumentParser
+from pathlib import Path
+from typing import Iterable
+
+from django_logs_analyzer.reports import handlers_report
+
+
+def argument_parser() -> ArgumentParser:
+    parser = ArgumentParser(description="Analyze Django logs.")
+    parser.add_argument(
+        "files",
+        nargs="+",
+        type=str,
+        help="Log files to analyze.",
+    )
+    parser.add_argument(
+        "-r",
+        "--report",
+        type=str,
+        default="handlers",
+        choices=["handlers"],
+        help="Type of report.",
+    )
+    return parser
+
+
+def find_nonexistent_files(files: list[str]) -> Iterable[str]:
+    return [file for file in files if not Path(file).exists()]
 
 
 if __name__ == "__main__":
-    log_lines = [
-        "2025-03-28 12:44:46,000 INFO django.request: GET /api/v1/reviews/ 204 OK [192.168.1.59]",
-        "2025-03-28 12:21:51,000 INFO django.request: GET /admin/dashboard/ 200 OK [192.168.1.68]",
-        "2025-03-28 12:40:47,000 CRITICAL django.core.management: DatabaseError: Deadlock detected",
-    ]
+    parser = argument_parser()
+    args = parser.parse_args()
 
-    for log in parse_logs(log_lines):
-        print(log)
+    if files := find_nonexistent_files(args.files):
+        parser.error(f"File(s) not found: {', '.join(files)}")
+    
+    match args.report:
+        case "handlers":
+            print(handlers_report(args.files))
