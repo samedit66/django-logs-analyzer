a56e6555fcaaf36358c06a4e87540115cb73f533
samedit66 <samedit66@yandex.ru>
2025-04-12 04:33:51 +0300

    fix: remove redundant 'django' prefix, add test with handler which is not connected with a GET request

--- tests/test_filter_request.py
+++ tests/test_filter_request.py
@@ -24,11 +24,18 @@ def test_filter_requests():
             source="core.management",
             message="DatabaseError: Deadlock detected",
         ),
+        DjangoLog(
+            time=datetime(2025, 3, 28, 12, 26, 26, 0),
+            level="ERROR",
+            source="request",
+            message="Internal Server Error: /api/v1/checkout/ [192.168.1.90] - ConnectionError: Failed to connect to payment gateway",
+        )
     ]
 
     expected_requests = [
         RequestLog(level="INFO", handler="/api/v1/reviews/"),
         RequestLog(level="INFO", handler="/admin/dashboard/"),
+        RequestLog(level="ERROR", handler="/api/v1/checkout/"),
     ]
 
     filtered_requests = list(filter_requests(logs))
